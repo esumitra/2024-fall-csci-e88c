b9ca33e514d82192085452545ce4c224a2684efb
tue372erdogdu <tue372@g.harvard.edu>
2024-09-16 01:05:08 -0500

    Week 2 first question

--- src/main/scala/org/cscie88c/week2/UtilFunctions.scala
+++ src/main/scala/org/cscie88c/week2/UtilFunctions.scala
@@ -3,7 +3,7 @@ package org.cscie88c.week2
 object UtilFunctions {
 
   // complete the functions below
-  def maximum(a: Int, b: Int): Int = ???
-  def average(a: Int, b: Int): Double = ???
+  def maximum(a: Int, b: Int): Int = List(a, b).max
+  def average(a: Int, b: Int): Double = { (a + b) / 2.0 }
 
 }
--- src/test/scala/org/cscie88c/week2/UtilFunctionsTest.scala
+++ src/test/scala/org/cscie88c/week2/UtilFunctionsTest.scala
@@ -3,16 +3,36 @@ package org.cscie88c.week2
 import org.cscie88c.testutils.{StandardTest}
 
 class UtilFunctionsTest extends StandardTest {
-  
+
   "UtilFunctions" when {
     "maximum" should {
       "return maximum of two ints when first integer is greater" in {
-        UtilFunctions.maximum(2, 1) should be (2)
+        UtilFunctions.maximum(2, 1) should be(2)
       }
       // add more unit tests for maximum below
-    }
+      "return maximum of two ints when second integer is greater" in {
+        UtilFunctions.maximum(4, 9) should be(9)
+      }
+      "return maximum of two ints when both integer is equal" in {
+        UtilFunctions.maximum(67, 67) should be(67)
+      }
 
-    // add unit tests for average below
-    
+    }
   }
+
+  // add unit tests for average below
+  "UtilFunctions" when {
+    "average" should {
+      "return average of two different integers(a and b; a<b)" in {
+        UtilFunctions.average(10, 18) should be(14.0)
+      }
+      "return average of two different integers(a and b; a>b)" in {
+        UtilFunctions.average(20, 15) should be(17.5)
+      }
+      "return average of two same integers(a and b; a=b)" in {
+        UtilFunctions.average(30, 30) should be(30.0)
+      }
+    }
+  }
+
 }
