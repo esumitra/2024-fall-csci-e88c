592c7ef21302d7d067eead5408c7fc02eb4f912b
tue372erdogdu <tue372@g.harvard.edu>
2024-09-16 02:08:25 -0500

    Week 2 Second Question

--- src/main/scala/org/cscie88c/week2/UniversityEmployee.scala
+++ src/main/scala/org/cscie88c/week2/UniversityEmployee.scala
@@ -1,3 +1,8 @@
 package org.cscie88c.week2
 
 // write the class UniversityEmployee below
+class UniversityEmployee(val name: String, val email: String) {
+
+  def description: String = s"Type: user, Name: ${name}, Email: ${email}"
+
+}
--- src/test/scala/org/cscie88c/week2/UniversityEmployeeTest.scala
+++ src/test/scala/org/cscie88c/week2/UniversityEmployeeTest.scala
@@ -4,11 +4,21 @@ import org.cscie88c.testutils.{StandardTest}
 
 // write unit tests for University employee below
 class UniversityEmployeeTest extends StandardTest {
+  val first_employee = new UniversityEmployee("Tufan", "tufan@gmail.com")
+  val second_employee = new UniversityEmployee("Erdogdu", "erdogdu@gmail.com")
   "UniversityEmployee" when {
     "instantiated" should {
       "have a name property" in {
-        true shouldBe true
+        first_employee.name should be("Tufan")
+        second_employee.name should be("Erdogdu")
       }
+      "have an email property" in {
+
+        first_employee.email should be("tufan@gmail.com")
+        second_employee.email should be("erdogdu@gmail.com")
+      }
+
     }
   }
+
 }
