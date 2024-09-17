4ef61b4e8c35b34f566f8bc9324a66e3ba06895a
tue372erdogdu <tue372@g.harvard.edu>
2024-09-16 23:06:29 -0500

    Week 2 Question 4 a

--- src/main/scala/org/cscie88c/week2/Student.scala
+++ src/main/scala/org/cscie88c/week2/Student.scala
@@ -1,14 +1,32 @@
 package org.cscie88c.week2
 
 // complete the definition of the Student case class and companion object
-final case class Student()
+final case class Student(
+    id: Int,
+    firstName: String,
+    lastName: String,
+    email: String,
+    gender: String,
+    country: String
+)
 
 object Student {
-  
+  def apply(csvTufan: String): Student = {
+    val values = csvTufan.split(",")
+    Student(
+      id = values(0).toInt,
+      firstName = values(1),
+      lastName = values(2),
+      email = values(3),
+      gender = values(4),
+      country = values(5)
+    )
+
+  }
   val allStudents: List[Student] = ???
 
   def studentNamesByCountry(country: String): List[String] = ???
 
   def studentTotalsByCountry(country: String): Int = ???
-  
+
 }
