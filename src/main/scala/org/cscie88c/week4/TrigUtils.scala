a52996304b953ffcfff7d5b7cf3d068dd7ff3a4c
esumitra <edward.sumitra@gmail.com>
2024-09-21 09:39:38 -0400

    week4 scaffolding

---
+++ src/main/scala/org/cscie88c/week4/TrigUtils.scala
@@ -0,0 +1,13 @@
+package org.cscie88c.week4
+
+object TrigUtils {
+
+  // https://www.scala-lang.org/api/2.13.6/scala/math/index.html
+  // use the function literal syntax for sin and cos
+  val sinDegrees: Double => Double = ???
+  val cosDegrees: Double => Double = ???
+
+  // use the placeholder syntax for squared
+  val squared: Double => Double = ???
+  
+}
